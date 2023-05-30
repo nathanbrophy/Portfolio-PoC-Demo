1ba505209792e43c1ebda39ccb7f8c3bf3bd2e70
Nathan Brophy <nathan.brophy@gmail.com>
2023-05-30 18:41:17 -0500

    initial ingress commit

--- deploy_poc.sh
+++ deploy_poc.sh
@@ -10,15 +10,6 @@ BLANK='\033[0m'
 RED='\033[1;31m'
 CYAN='\033[1;36m'
 
-function build_and_push {
-    printf "${_SKIP_BUILD}" | grep '[tT][rR][uU][eE]' > /dev/null && return
-
-    cmd="make docker-build ${1}"
-    eval "${cmd}"
-    cmd="make docker-push  ${1}"
-    eval "${cmd}"
-}
-
 function info {
     log "${CYAN}INFO${BLANK}" "${1}"
 }
@@ -38,29 +29,10 @@ function verify_requisite {
 }
 
 function check_prereqs {
-    local reqs=(docker go kubectl terraform yq aws eksctl)
+    local reqs=(kubectl yq aws)
     for r in "${reqs[@]}"; do 
         verify_requisite "${r}"
     done
-}
-
-function check_env_settings {
-    local vars=(AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY)
-    for v in in "${vars[@]}"; do 
-        env | grep "${v}" > /dev/null || error "required environment setting ${v} not found"
-    done
-}
-
-function server {
-    info "building and deploying the server to remote registry"
-
-    local make_flags="VERSION=$(cat deploy_config.yaml | yq -Mr '.deploy.server.tag') IMAGE_REPO=$(cat deploy_config.yaml | yq -Mr '.deploy.server.repository') IMAGE_REGISTRY=$(cat deploy_config.yaml | yq -Mr '.deploy.registry') OS=$(cat deploy_config.yaml | yq -Mr '.deploy.os') ARCH=$(cat deploy_config.yaml | yq -Mr '.deploy.arch')"
-
-    cd server
-
-    build_and_push "${make_flags}"
-
-    cd "${ROOT_DIR}"
 }
 
 function controller {
@@ -75,11 +47,7 @@ function controller {
     local server_image="${image_registry}/${server_repository}:${server_tag}"
     server_image="$(printf ${server_image} | tr -d ' ')"
 
-    local make_flags="IMG=${image}"
-
     cd k8s
-
-    build_and_push "${make_flags}"
 
     info "installing CRDs to cluster"
 
@@ -87,7 +55,7 @@ function controller {
 
     info "deploying controller to environment"
 
-    kubectl kustomize config/default | sed "s%REPLACE_IMAGE%${image_registry}/${image_repository}:${image_tag}%g" | kubectl apply -f -
+    kubectl kustomize config/default | sed "s%REPLACE_IMAGE%${image}%g" | kubectl apply -f -
 
     info "create the CR to deploy the application to the environment"
 
@@ -104,41 +72,6 @@ spec:
 EOF
 
     cd "${ROOT_DIR}"
-}
-
-function infrastructure {
-    info "deploying the infrastructure to the cloud provider"
-
-    local work_dir="$(mktemp -d)"
-    local tf_plan_file="${work_dir}/state.tfplan"
-    local region="$(cat deploy_config.yaml | yq -Mr '.deploy.terraform.region')"
-    local cluster_name="$(cat deploy_config.yaml | yq -Mr '.deploy.terraform.cluster_name')"
-
-    info "the working directory for the terraform plan is ${work_dir}"
-
-    cd "infrastructure/aws"
-
-    terraform plan -no-color -var region="${region}" -var cluster_name="${cluster_name}" > "${tf_plan_file}"
-
-    # Block operation of the apply until the plan is approved
-    echo 'Please press any key to continue and review the terraform plan'
-    read DUMMY
-
-    cat "${tf_plan_file}" | less
-
-    echo "Conintue [y/N]: "
-    read CONTINUE
-
-    [[ "${CONTINUE}" != "y" ]] && error "user did not accept the terraform plan, canceling apply"
-
-    [[ "${CRITICAL_ERROR}" -eq 1 ]] && exit 1
-
-    terraform apply -var region="${region}" -var cluster_name="${cluster_name}" -auto-approve
-
-    cd "${ROOT_DIR}"
-    info "done standing up the AWS infrastructure"
-
-    rm -rf "${work_dir}"
 }
 
 function configure_cluster_credentials {
@@ -240,20 +173,12 @@ function run {
     info "Version ${VERSION}"
 
     check_prereqs 
-    check_env_settings
 
     [[ "${CRITICAL_ERROR}" -eq 1 ]] && exit 1
 
     info "all prerequisites have been verified"
 
-    server
-    infrastructure
     configure_cluster_credentials
-
-    info "checking to see if load balancer requires instalation"
-    cat deploy_config.yaml | yq -Mr '.deploy.load_balancer.deploy' | grep '[tT][rR][uU][eE]' > /dev/null &&
-    setup_load_balancer
-
     context_namespace
     controller
 
